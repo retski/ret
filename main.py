c4d7912b250fd105fe408871101e728a0183f4ef
Ethan <54964352+retski@users.noreply.github.com>
2022-02-18 14:01:28 +0000

    Added file posting + deleting afterwards

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Main Bot
 from __future__ import unicode_literals
 import os
-
+import json
 import discord
 import yt_dlp
+
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 
 class YTLogger(object):
@@ -21,7 +23,7 @@ class YTLogger(object):
 
 ydl_opts = {
     'format': 'bestvideo[ext=mp4]/mp4',
-    'outputmpl': '%(title)s',
+    'outtmpl': '%(title)s.mp4',
     'logger': YTLogger()
 }
 # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -34,9 +36,22 @@ bot = commands.Bot(command_prefix=';')
 async def on_ready():
     print(f'Connected as {bot.user}.')
 
-@bot.command(name='ret')
+@bot.command(name='ret', pass_context=True)
 async def mainCmd(ctx, arg):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(arg)
         ydl.download([arg])
 
+        filename = 'info.json'
+        with open(filename, 'w') as file_object:
+            json.dump(info, file_object)
+
+        f = open('info.json')
+        data = json.load(f)
+
+        title = data['title']
+        print(title)
+        area=ctx.message.channel
+        await area.send(file = discord.File(f"{title}.mp4"))
+        os.remove(f"{title}.mp4")
 bot.run(TOKEN)
