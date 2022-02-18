10ca084a09bb7beee89b6caae5e7da6a5505f418
Ethan <54964352+retski@users.noreply.github.com>
2022-02-18 13:08:39 +0000

    Working on downloading + user input

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 # Main Bot
+from __future__ import unicode_literals
 import os
 
 import discord
+import youtube_dl
 from dotenv import load_dotenv
+from discord.ext import commands
 
+
+class YTLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+ydl_opts = {
+    'format': 'bestvideo[ext=mp4]/mp4',
+    'outputmpl': '%(title)s',
+    'logger': YTLogger(),
+    'download_archive': '/video'
+}
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
+TOKEN = os.getenv('DISCORD_TOKEN') # Grabs discord token from .env file
+bot = commands.Bot(command_prefix=';')
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'Connected as {client.user}.')
+    print(f'Connected as {bot.user}.')
 
+@bot.command(name='ret')
+async def mainCmd(ctx, arg):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([arg])
 
-client.run(TOKEN)
+bot.run(TOKEN)
