d41fc5f3c01fef820cdadc03db60afaf306ac624
Ethan <54964352+retski@users.noreply.github.com>
2022-10-17 10:12:55 +0100

    Minor updates and fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Main Bot
 from __future__ import unicode_literals
-import os, json, discord, yt_dlp, re, ffmpeg, traceback, logging, sys, asyncio, json
+import os, json, discord, yt_dlp, re, ffmpeg, traceback, logging, sys, asyncio, json, random
+from random import choice
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -29,11 +30,11 @@ sys.excepthook = exception_handler
 
 
 ydl_opts = {
+    'max_filesize': 100000000,
     'format': 'bestvideo[ext=mp4]+bestaudio/bestvideo[ext=mp4]/bestaudio/best',
     'merge_output_format': 'mp4',
     'outtmpl': 'ret.mp4',
-    'max-filesize': '100m',
-    'logger': exception_handler(),
+    'logger': exception_handler()
 }
 # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 load_dotenv()
@@ -59,10 +60,6 @@ async def on_ready(): # Event occurs as soon as bot is online
 async def on_command(message):
     if message.author == bot.user:
         return
-    banned = [969117255965229086, 329400132325867530]
-    if message.author.id in banned:
-        await message.channel.send(f'{message.author.mention} fk off')
-        return
 
 @bot.command(name='ret', pass_context=True) # Command ;ret
 @commands.guild_only()
@@ -78,145 +75,151 @@ async def mainCmd(ctx, arg): # Passes message context and stores the 1st part as
         if retYT.is_file():
             os.remove('ret_yt.mp4')
 
+
     finally:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl: # yt-dlp download with ydl_opts as options
-                await ctx.channel.trigger_typing()
-                ydl.download([arg])
-                info = ydl.extract_info(arg) # stores arg info in json format to 'info' variable
-                filename = 'info.json' # creating a json file for info
-                with open(filename, 'w') as file_object:
-                    json.dump(info, file_object)
-                f = open('info.json')
-                data = json.load(f)
-                if "twitter" in arg: # Each site has different json info, so an if is needed for each
-                    await ctx.message.delete()
-                    title = data['title'] 
-                    uploader = data['uploader_id']
-                    uploader_url = data['uploader_url']
-                    likes = data['like_count']
-                    reposts = data['repost_count']
-                    comments = data['comment_count']
-                    file = discord.File("ret.mp4")
-                    embed = discord.Embed(
-                        title = remove_urls(f"{title}"),
-                        url = arg,
-                        color = discord.Color.blue())
-                    embed.set_author(
-                        name = (f"{uploader}"),
-                        url = (f"{uploader_url}"),
-                        icon_url = "https://cdn-icons-png.flaticon.com/512/124/124021.png")
-                    embed.set_footer(
-                        text=(f"❤️ {likes} 🔁 {reposts} 💬 {comments} • {ctx.author}"))
-                    process = ffmpeg.input('ret.mp4')
-                    process = ffmpeg.output(process, 'ret_tw.mp4', vcodec='libx264')
-                    ffmpeg.run(process)
-                    ffmpeg.overwrite_output(process)
-                    file = discord.File("ret_tw.mp4")
-                    await ctx.send(file=file, embed=embed)
-                    logging.info(f"Downloading {title} for {ctx.author}...")
-                    os.remove("ret.mp4")
-                    os.remove("ret_tw.mp4")
-                elif "youtube" in arg or "youtu" in arg:
-                    await ctx.message.delete()
-                    title = data['title'] 
-                    uploader = data['uploader']
-                    uploader_url = data['uploader_url']
-                    likes = data['like_count']
-                    views = data['view_count']
-                    file = discord.File("ret.mp4")
-                    embed = discord.Embed(
-                        title = remove_urls(f"{title}"),
-                        url = arg,
-                        color = discord.Color.red())
-                    embed.set_author(
-                        name = (f"{uploader}"),
-                        url = (f"{uploader_url}"),
-                        icon_url = "https://brandlogos.net/wp-content/uploads/2020/03/YouTube-icon-SVG-512x512.png")
-                    embed.set_footer(
-                        text=(f"❤️ {likes} 👁️ {views} • {ctx.author}"))
-                    process = ffmpeg.input('ret.mp4')
-                    process = ffmpeg.output(process, 'ret_yt.mp4', vcodec='libx264')
-                    ffmpeg.run(process)
-                    ffmpeg.overwrite_output(process)
-                    file = discord.File("ret_yt.mp4")
-                    await ctx.send(file=file, embed=embed)
-                    logging.info(f"Downloading {title} for {ctx.author}...")
-                    os.remove("ret_yt.mp4")
-                    os.remove("ret.mp4")
-                elif "tiktok" in arg:
-                    await ctx.message.delete()
-                    title = data['title'] 
-                    uploader = data['creator']
-                    uploader_url = data['uploader_url']
-                    likes = data['like_count']
-                    reposts = data['repost_count']
-                    comments = data['comment_count']
-                    embed = discord.Embed(
-                        title = remove_urls(f"{title}"),
-                        url = arg,
-                        color = discord.Color.purple())
-                    embed.set_author(
-                        name = (f"{uploader}"),
-                        url = (f"{uploader_url}"),
-                        icon_url = "https://cliply.co/wp-content/uploads/2021/02/372102780_TIKTOK_ICON_1080.png")
-                    embed.set_footer(
-                        text=(f"❤️ {likes} 🔁 {reposts} 💬 {comments} • {ctx.author}"))
-                    process = ffmpeg.input('ret.mp4')
-                    process = ffmpeg.output(process, 'ret_tt.mp4', vcodec='libx264')
-                    ffmpeg.run(process)
-                    ffmpeg.overwrite_output(process)
-                    file = discord.File("ret_tt.mp4")
-                    await ctx.send(file=file, embed=embed)
-                    logging.info(f"Downloading {title} for {ctx.author}...")
-                    os.remove("ret_tt.mp4")
-                    os.remove("ret.mp4")
+        banned = [969117255965229086, 329400132325867530, 659358912281182219]
+        if ctx.author.id in banned:
+            await ctx.channel.send(f'{ctx.author.mention} fk off')
+            return
+        else:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl: # yt-dlp download with ydl_opts as options
+                    await ctx.channel.trigger_typing()
+                    ydl.download([arg])
+                    info = ydl.extract_info(arg) # stores arg info in json format to 'info' variable
+                    filename = 'info.json' # creating a json file for info
+                    with open(filename, 'w') as file_object:
+                        json.dump(info, file_object)
+                    f = open('info.json')
+                    data = json.load(f)
+                    if "twitter" in arg: # Each site has different json info, so an if is needed for each
+                        await ctx.message.delete()
+                        title = data['title'] 
+                        uploader = data['uploader_id']
+                        uploader_url = data['uploader_url']
+                        likes = data['like_count']
+                        reposts = data['repost_count']
+                        comments = data['comment_count']
+                        file = discord.File("ret.mp4")
+                        embed = discord.Embed(
+                            title = remove_urls(f"{title}"),
+                            url = arg,
+                            color = discord.Color.blue())
+                        embed.set_author(
+                            name = (f"{uploader}"),
+                            url = (f"{uploader_url}"),
+                            icon_url = "https://cdn-icons-png.flaticon.com/512/124/124021.png")
+                        embed.set_footer(
+                            text=(f"❤️ {likes} 🔁 {reposts} 💬 {comments} • {ctx.author}"))
+                        process = ffmpeg.input('ret.mp4')
+                        process = ffmpeg.output(process, 'ret_tw.mp4', vcodec='libx264')
+                        ffmpeg.run(process)
+                        ffmpeg.overwrite_output(process)
+                        file = discord.File("ret_tw.mp4")
+                        await ctx.send(file=file, embed=embed)
+                        logging.info(f"Downloading {title} for {ctx.author}...")
+                        os.remove("ret.mp4")
+                        os.remove("ret_tw.mp4")
+                    elif "youtube" in arg or "youtu" in arg:
+                        await ctx.message.delete()
+                        title = data['title'] 
+                        uploader = data['uploader']
+                        uploader_url = data['uploader_url']
+                        likes = data['like_count']
+                        views = data['view_count']
+                        file = discord.File("ret.mp4")
+                        embed = discord.Embed(
+                            title = remove_urls(f"{title}"),
+                            url = arg,
+                            color = discord.Color.red())
+                        embed.set_author(
+                            name = (f"{uploader}"),
+                            url = (f"{uploader_url}"),
+                            icon_url = "https://brandlogos.net/wp-content/uploads/2020/03/YouTube-icon-SVG-512x512.png")
+                        embed.set_footer(
+                            text=(f"❤️ {likes} 👁️ {views} • {ctx.author}"))
+                        process = ffmpeg.input('ret.mp4')
+                        process = ffmpeg.output(process, 'ret_yt.mp4', vcodec='libx264')
+                        ffmpeg.run(process)
+                        ffmpeg.overwrite_output(process)
+                        file = discord.File("ret_yt.mp4")
+                        await ctx.send(file=file, embed=embed)
+                        logging.info(f"Downloading {title} for {ctx.author}...")
+                        os.remove("ret_yt.mp4")
+                        os.remove("ret.mp4")
+                    elif "tiktok" in arg:
+                        await ctx.message.delete()
+                        title = data['title'] 
+                        uploader = data['creator']
+                        uploader_url = data['uploader_url']
+                        likes = data['like_count']
+                        reposts = data['repost_count']
+                        comments = data['comment_count']
+                        embed = discord.Embed(
+                            title = remove_urls(f"{title}"),
+                            url = arg,
+                            color = discord.Color.purple())
+                        embed.set_author(
+                            name = (f"{uploader}"),
+                            url = (f"{uploader_url}"),
+                            icon_url = "https://cliply.co/wp-content/uploads/2021/02/372102780_TIKTOK_ICON_1080.png")
+                        embed.set_footer(
+                            text=(f"❤️ {likes} 🔁 {reposts} 💬 {comments} • {ctx.author}"))
+                        process = ffmpeg.input('ret.mp4')
+                        process = ffmpeg.output(process, 'ret_tt.mp4', vcodec='libx264')
+                        ffmpeg.run(process)
+                        ffmpeg.overwrite_output(process)
+                        file = discord.File("ret_tt.mp4")
+                        await ctx.send(file=file, embed=embed)
+                        logging.info(f"Downloading {title} for {ctx.author}...")
+                        os.remove("ret_tt.mp4")
+                        os.remove("ret.mp4")
 
-                elif "reddit" in arg or "redd" in arg:
-                    await ctx.message.delete()
-                    title = data['title']
-                    uploader = data['uploader']
-                    likes = data['like_count']
-                    comments = data['comment_count']
-                    embed = discord.Embed(
-                        title = f"{title}",
-                        url = arg,
-                        color = discord.Color.red())
-                    embed.set_author(
-                        name = (f"{uploader}"),
-                        icon_url = "https://external-preview.redd.it/iDdntscPf-nfWKqzHRGFmhVxZm4hZgaKe5oyFws-yzA.png?width=640&crop=smart&auto=webp&s=bfd318557bf2a5b3602367c9c4d9cd84d917ccd5")
-                    embed.set_footer(
-                        text=(f"❤️ {likes} 💬 {comments} • {ctx.author}"))
-                    file = discord.File("ret.mp4")
-                    await ctx.send(file=file, embed=embed)
-                    os.remove("ret.mp4")
+                    elif "reddit" in arg or "redd" in arg:
+                        await ctx.message.delete()
+                        title = data['title']
+                        uploader = data['uploader']
+                        likes = data['like_count']
+                        comments = data['comment_count']
+                        embed = discord.Embed(
+                            title = f"{title}",
+                            url = arg,
+                            color = discord.Color.red())
+                        embed.set_author(
+                            name = (f"{uploader}"),
+                            icon_url = "https://external-preview.redd.it/iDdntscPf-nfWKqzHRGFmhVxZm4hZgaKe5oyFws-yzA.png?width=640&crop=smart&auto=webp&s=bfd318557bf2a5b3602367c9c4d9cd84d917ccd5")
+                        embed.set_footer(
+                            text=(f"❤️ {likes} 💬 {comments} • {ctx.author}"))
+                        file = discord.File("ret.mp4")
+                        await ctx.send(file=file, embed=embed)
+                        os.remove("ret.mp4")
 
-                elif "instagram" in arg:
-                    await ctx.message.delete()
-                    title = data['title']
-                    uploader = data['channel']
-                    likes = data['like_count']
-                    comments = data['comment_count']
-                    embed = discord.Embed(
-                        title = f"{title}",
-                        url = arg,
-                        color = discord.Color.red())
-                    embed.set_author(
-                        name = (f"{uploader}"),
-                        icon_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png")
-                    embed.set_footer(
-                        text=(f"❤️ {likes} 💬 {comments} • {ctx.author}"))
-                    file = discord.File("ret.mp4")
-                    await ctx.send(file=file, embed=embed)
-                    os.remove("ret.mp4")
-                else:
+                    elif "instagram" in arg:
+                        await ctx.message.delete()
+                        title = data['title']
+                        uploader = data['channel']
+                        likes = data['like_count']
+                        comments = data['comment_count']
+                        embed = discord.Embed(
+                            title = f"{title}",
+                            url = arg,
+                            color = discord.Color.red())
+                        embed.set_author(
+                            name = (f"{uploader}"),
+                            icon_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png")
+                        embed.set_footer(
+                            text=(f"❤️ {likes} 💬 {comments} • {ctx.author}"))
+                        file = discord.File("ret.mp4")
+                        await ctx.send(file=file, embed=embed)
+                        os.remove("ret.mp4")
+                    else:
 
-                    embed = discord.Embed(
-                        title = "⚠️ Site wasn't recognised",
-                        description = "Attempting download anyway",
-                        color = discord.Color.dark_gold())
-                    file = discord.File("ret.mp4")
-                    await ctx.send(file=file, embed=embed)
-                    os.remove("ret.mp4")
+                        embed = discord.Embed(
+                            title = "⚠️ Site wasn't recognised",
+                            description = "Attempting download anyway",
+                            color = discord.Color.dark_gold())
+                        file = discord.File("ret.mp4")
+                        await ctx.send(file=file, embed=embed)
+                        os.remove("ret.mp4")
 
 @bot.command(name='help', pass_context=True)
 @commands.guild_only()
@@ -236,6 +239,23 @@ def _save():
 @has_permissions(administrator=True)
 async def save(ctx):
     _save()
+
+@bot.command(name='flip', pass_context=True) # Command ;ret
+@commands.guild_only()
+async def flip(ctx):
+    determine_flip = [1, 0]
+    banned = [969117255965229086, 329400132325867530, 659358912281182219]
+    if ctx.author.id in banned:
+        await ctx.channel.send(f'{ctx.author.mention} fk off')
+        return
+    else:
+        if random.choice(determine_flip) == 1:
+            embed = discord.Embed(title="Coinflip", description=f"{ctx.author.mention} Flipped a coin, you got **Heads**!", color=discord.Color.from_rgb(138, 94, 40))
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(title="Coinflip", description=f"{ctx.author.mention} Flipped a coin, you got **Tails**!", color=discord.Color.from_rgb(138, 94, 40))
+            await ctx.send(embed=embed)
 """
 @bot.command(name='channel', pass_context=True)
 @commands.guild_only()
